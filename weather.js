a1b8c8b68db8d7ae9a9d03e15fbf08bcb1ab9dcc
Kedar Nadkarny <kedar.nadkarny@gmail.com>
2016-09-02 18:25:19 -0500

    project setup

---
+++ weather.js
@@ -0,0 +1,41 @@
+// Put your Last.fm API key here
+/*var api_key = "";
+
+function sendRequest () {
+    var xhr = new XMLHttpRequest();
+    var method = "artist.getinfo";
+    var artist = encodeURI(document.getElementById("form-input").value);
+    xhr.open("GET", "proxy.php?method="+method+"&artist="+artist+"&api_key="+api_key+"&format=json", true);
+    xhr.setRequestHeader("Accept","application/json");
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            var json = JSON.parse(this.responseText);
+            var str = JSON.stringify(json,undefined,2);
+            document.getElementById("output").innerHTML = "<pre>" + str + "</pre>";
+        }
+    };
+    xhr.send(null);
+}*/
+
+
+var api_key = "4a1feb2b6466b332b5b12804e9935875";
+
+function sendRequest () {
+    var xhr = new XMLHttpRequest();
+   // var method = "artist.getinfo";
+    var city = encodeURI(document.getElementById("form-input").value);
+    xhr.open("GET", "proxy.php?q="+city+"&appid="+api_key+"&format=json", true);
+    xhr.setRequestHeader("Accept","application/json");
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            var json = JSON.parse(this.responseText);
+            var str = JSON.stringify(json,undefined,2);
+            //document.getElementById("output").innerHTML = "<pre>" + str + "</pre>";
+
+            obj = JSON.parse(str);
+            document.getElementById("output").innerHTML = "<pre>" + obj + "</pre>";
+            console.log(obj);
+        }
+    };
+    xhr.send(null);
+}
